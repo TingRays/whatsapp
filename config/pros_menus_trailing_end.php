b1a101e42bfb3fdfbfef89a656be81c33b07498c
Aaron Ting <destiny5goddess@163.com>
2022-11-05 19:10:52 +0800

    commit

---
+++ config/pros_menus_trailing_end.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Power by abnermouke/pros.
+ * User: Abnermouke <abnermouke@outlook.com>
+ * Originate in YunniTec.
+ */
+
+return [
+
+
+    /*
+   |--------------------------------------------------------------------------
+   | Pros default menus setting
+   |--------------------------------------------------------------------------
+   |
+   | The default pros menus settings
+   |
+   */
+
+    [
+
+        [
+            'guard_name' => '控制台',
+            'handler' => '',
+            'route' => ['name' => 'pros.console.index', 'params' => []],
+            'permission_nodes' => [],
+            'icon' => '',
+            'group_menus' => [],
+        ],
+        [
+            'guard_name' => '粉丝管理',
+            'handler' => '',
+            'route' => ['name' => 'whatsapp.console.fans_manage.index', 'params' => []],
+            'permission_nodes' => [],
+            'icon' => '',
+            'group_menus' => [
+                [
+                    [
+                        'guard_name' => '我的粉',
+                        'handler' => '',
+                        'route' => ['name' => 'whatsapp.console.fans_manage.index', 'params' => []],
+                        'permission_nodes' => [],
+                        'icon' => 'fa fa-asterisk',
+                    ],
+                    [
+                        'guard_name' => '粉的管理分组',
+                        'handler' => '',
+                        'route' => ['name' => 'whatsapp.console.fans_manage.group.index', 'params' => []],
+                        'permission_nodes' => [],
+                        'icon' => 'fa fa-id-card-alt',
+                    ],
+                ]
+            ],
+        ],
+    ],
+
+];
--- resources/views/pros/console/layouts/header.blade.php
+++ resources/views/pros/console/layouts/header.blade.php
@@ -25,10 +25,19 @@
             </span>
         </div>
         <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
+            @php
+                $current_auth = current_auth(false, config('pros.session_prefix'));
+                $role_alias = data_get($current_auth, 'role_alias', 'administrator');
+                if($role_alias === 'trailing_end'){
+                    $pros_menus = config('pros_menus_trailing_end', []);
+                }else{
+                    $pros_menus = config('pros_menus', []);
+                }
+            @endphp
             <div class="d-flex align-items-stretch" id="kt_header_nav">
                 <div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
                     <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch pros_header_menus_box" id="#kt_header_menu" data-kt-menu="true">
-                        @foreach(config('pros_menus', []) as $menus)
+                        @foreach($pros_menus as $menus)
                             @foreach($menus as $menu)
                                 <div data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" class="menu-item menu-lg-down-accordion me-lg-1 pros_header_menus_items">
                                     <span class="menu-link py-3">
@@ -60,9 +69,6 @@
                     </div>
                 </div>
             </div>
-            @php
-                $current_auth = current_auth(false, config('pros.session_prefix'));
-            @endphp
             <div class="d-flex align-items-stretch flex-shrink-0">
                 <div class="d-flex align-items-center ms-1">
                     @if(pros_console_has_permission('pros.console.systems.refresh.nodes', 'post'))
