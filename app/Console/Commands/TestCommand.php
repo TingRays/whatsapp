b037628300cfc78a80a65c00c4cf2aa5c40e816b
Aaron Ting <destiny5goddess@163.com>
2022-11-11 02:27:58 +0800

    commit

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -39,16 +39,6 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        $account_info = (new AccountRepository())->get(['global_roaming'=>55],['id','global_roaming','mobile']);
-        foreach ($account_info as $item){
-            $mobile = $item['mobile'];
-            $global_roaming = $item['global_roaming'];
-            if (strpos($mobile,$global_roaming) === 0){
-                $global_roaming_len = strlen($global_roaming);
-                $mobile = substr($mobile,$global_roaming_len);
-                (new AccountRepository())->update(['id'=>$item['id']],['mobile'=>$mobile]);
-            }
-        }
         // TODO：测试内容
         //$access_token = 'EAAG7HNVhnqIBAOtPfOFbZAYv9c3H3mneXxSrPN3ZCUiUY5aHInTycjMP4LhaUP8fi9lgfGPj14InjIZCBLTmfqP8BYZBC91Irulvz9s0SAMQr3R6PuLcylo0eTycLmEV3jRtvBl9IoSQTDBDf9RntFyrX2sJ61kabkVfT7Xr12uItrAlkEpAzu1O6W1ZBbzPoI8hVn5cijaIgZCc3gixaRkQEP6alGpdMZD';
         //dd((new CloudApiImplementers('100785316177486',$access_token))->sendText());
