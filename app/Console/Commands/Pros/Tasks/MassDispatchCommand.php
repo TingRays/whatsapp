e0ec0a47d73d280aa72aa3ade036d38ed2cc9a57
Aaron Ting <destiny5goddess@163.com>
2022-11-13 16:22:48 +0800

    commit

--- app/Console/Commands/Pros/Tasks/MassDispatchCommand.php
+++ app/Console/Commands/Pros/Tasks/MassDispatchCommand.php
@@ -87,12 +87,12 @@ class MassDispatchCommand extends Command
         if ($workers > 10){
             $workers = 10;
         }
-        for ($i = 0; $i < $workers; $i++) {
+        for ($i = 1; $i <= $workers; $i++) {
             //查询可以用于发送消息的商户
             $merchant = (new MerchantRepository())->row(['remainder'=>['>',0],'status'=>Merchants::STATUS_ENABLED],['id','remainder','tel_code','auth_token']);
             if (empty($merchant)){
                 //没有商户可以用于发送消息
-                return;
+                break;
             }
             //更新商户状态
             (new MerchantRepository())->update(['id'=>$merchant['id']],['status'=>Merchants::STATUS_VERIFYING,'updated_at'=>auto_datetime()]);
@@ -102,62 +102,62 @@ class MassDispatchCommand extends Command
             $remainder = $default_size = $merchant['remainder'];
             //等待发送中
             $message_logs = (new MerchantMessagesLogRepository())->limit(['merchant_messages_id'=>$merchant_message_id,'status'=>MerchantMessagesLogs::STATUS_DISABLED,'mode'=>MerchantMessagesLogs::MODE_OF_MERCHANT],['id','account_id','merchant_messages_id','type','template_id'],[],['id'=>'desc'],[],1,$default_size);
-            if ($message_logs){
-                //更新消息发送状态 - 发送中
-                $message_log_ids = array_column($message_logs,'id','id');
-                (new MerchantMessagesLogRepository())->update(['id'=>['in',$message_log_ids]],['status'=>MerchantMessagesLogs::STATUS_VERIFYING,'updated_at'=>auto_datetime()]);
-                //获取模板
-                $template_ids = array_column($message_logs,'template_id');
-                $templates = (new MerchantTemplateRepository())->get(['id'=>['in',$template_ids]],['id','title','language','header_type','header_content','body','button']);
-                $templates = array_column($templates, null, 'id');
-                //获取用户
-                $account_ids = array_column($message_logs,'account_id');
-                $accounts = (new AccountRepository())->get(['id'=>['in',$account_ids]],['id','global_roaming','mobile']);
-                $accounts = array_column($accounts,null,'id');
-                // 创建子进程
-                $process_ids[$i] = pcntl_fork();
-                switch ($process_ids[$i]) {
-                    case -1 :
-                        echo "fork failed : {$i} \r\n";
-                        exit;
-                    case 0 :
-                        // 子进程处理消息发送
-                        foreach ($message_logs as $k=>$message_log){
-                            $accounts_info = $accounts[$message_log['account_id']]??['global_roaming'=>0,'mobile'=>0];
-                            $to_mobile = $accounts_info['global_roaming'].$accounts_info['mobile'];
-                            //模板发送成功
-                            $result = (new CloudApiImplementers($merchant['tel_code'],$merchant['auth_token']))->sendTextTemplate($templates[$message_log['template_id']]??[],$to_mobile);
-                            (new MerchantMessagesLogRepository())->update(['id'=>$message_log['id']],
-                                ['merchant_id'=>$merchant['id'],'content'=>$result['data']??[],'result'=>$result['result']??[],
-                                    'status'=>MerchantMessagesLogs::STATUS_ENABLED,'updated_at'=>auto_datetime()]);
-                            $remainder--;
-                            unset($message_logs[$k]);
-                            unset($message_log_ids[$message_log['id']]);
-                            if ($remainder <= 0){
-                                break;
-                            }
-                        }
-                        (new MerchantRepository())->update(['id'=>$merchant['id']],['remainder'=>$remainder,'status'=>Merchants::STATUS_ENABLED,'updated_at'=>auto_datetime()]);
-                        //恢复未处理的
-                        if ($message_log_ids){
-                            //等待发送中
-                            (new MerchantMessagesLogRepository())->update(['id'=>['id',$message_log_ids]],['status'=>MerchantMessagesLogs::STATUS_DISABLED,'updated_at'=>auto_datetime()]);
-                        }
-                        //更新消息发送状态 - 发送完成
-                        $not_send_num = (new MerchantMessagesLogRepository)->count(['merchant_messages_id'=>$merchant_message_id,'status'=>['in',[MerchantMessagesLogs::STATUS_DISABLED,MerchantMessagesLogs::STATUS_VERIFYING]]]);
-                        if ($not_send_num <= 0){
-                            //消息发送完毕
-                            (new MerchantMessageRepository())->update(['id'=>$merchant_message_id],['status'=>MerchantMessages::STATUS_ENABLED,'updated_at'=>auto_datetime()]);
-                        }else{
-                            //恢复状态
-                            (new MerchantMessageRepository())->update(['id'=>$merchant_message_id],['status'=>MerchantMessages::STATUS_DISABLED,'updated_at'=>auto_datetime()]);
-                        }
-                        exit;
-                    default :
-                        break;
-                }
+            if (empty($message_logs)){
+                break;
             }
-
+            //更新消息发送状态 - 发送中
+            $message_log_ids = array_column($message_logs,'id','id');
+            (new MerchantMessagesLogRepository())->update(['id'=>['in',$message_log_ids]],['status'=>MerchantMessagesLogs::STATUS_VERIFYING,'updated_at'=>auto_datetime()]);
+            //获取模板
+            $template_ids = array_column($message_logs,'template_id');
+            $templates = (new MerchantTemplateRepository())->get(['id'=>['in',$template_ids]],['id','title','language','header_type','header_content','body','button']);
+            $templates = array_column($templates, null, 'id');
+            //获取用户
+            $account_ids = array_column($message_logs,'account_id');
+            $accounts = (new AccountRepository())->get(['id'=>['in',$account_ids]],['id','global_roaming','mobile']);
+            $accounts = array_column($accounts,null,'id');
+            // 创建子进程
+            $process_ids[$i] = pcntl_fork();
+            switch ($process_ids[$i]) {
+                case -1 :
+                    echo "fork failed : {$i} \r\n";
+                    exit;
+                case 0 :
+                    // 子进程处理消息发送
+                    foreach ($message_logs as $k=>$message_log){
+                        $accounts_info = $accounts[$message_log['account_id']]??['global_roaming'=>0,'mobile'=>0];
+                        $to_mobile = $accounts_info['global_roaming'].$accounts_info['mobile'];
+                        //模板发送成功
+                        $result = (new CloudApiImplementers($merchant['tel_code'],$merchant['auth_token']))->sendTextTemplate($templates[$message_log['template_id']]??[],$to_mobile);
+                        (new MerchantMessagesLogRepository())->update(['id'=>$message_log['id']],
+                            ['merchant_id'=>$merchant['id'],'content'=>$result['data']??[],'result'=>$result['result']??[],
+                                'status'=>MerchantMessagesLogs::STATUS_ENABLED,'updated_at'=>auto_datetime()]);
+                        $remainder--;
+                        unset($message_logs[$k]);
+                        unset($message_log_ids[$message_log['id']]);
+                        if ($remainder <= 0){
+                            break;
+                        }
+                    }
+                    (new MerchantRepository())->update(['id'=>$merchant['id']],['remainder'=>$remainder,'status'=>Merchants::STATUS_ENABLED,'updated_at'=>auto_datetime()]);
+                    //恢复未处理的
+                    if ($message_log_ids){
+                        //等待发送中
+                        (new MerchantMessagesLogRepository())->update(['id'=>['id',$message_log_ids]],['status'=>MerchantMessagesLogs::STATUS_DISABLED,'updated_at'=>auto_datetime()]);
+                    }
+                    //更新消息发送状态 - 发送完成
+                    $not_send_num = (new MerchantMessagesLogRepository)->count(['merchant_messages_id'=>$merchant_message_id,'status'=>['in',[MerchantMessagesLogs::STATUS_DISABLED,MerchantMessagesLogs::STATUS_VERIFYING]]]);
+                    if ($not_send_num <= 0){
+                        //消息发送完毕
+                        (new MerchantMessageRepository())->update(['id'=>$merchant_message_id],['status'=>MerchantMessages::STATUS_ENABLED,'updated_at'=>auto_datetime()]);
+                    }else{
+                        //恢复状态
+                        (new MerchantMessageRepository())->update(['id'=>$merchant_message_id],['status'=>MerchantMessages::STATUS_DISABLED,'updated_at'=>auto_datetime()]);
+                    }
+                    exit;
+                default :
+                    break;
+            }
         }
         //子进程完成之后要退出
         while (count($process_ids) > 0) {
