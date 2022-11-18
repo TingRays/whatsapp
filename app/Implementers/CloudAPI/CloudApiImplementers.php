bf65f2fb38ff751dd3915b5b63f16aab9e6dcf5d
Aaron Ting <destiny5goddess@163.com>
2022-11-18 23:14:34 +0800

    commit

--- app/Implementers/CloudAPI/CloudApiImplementers.php
+++ app/Implementers/CloudAPI/CloudApiImplementers.php
@@ -140,7 +140,7 @@ class CloudApiImplementers extends BaseService
                     ],
                 ];
                 break;
-            case 'notificao_pt_BR':
+            case 'moban1_pt_BR':
                 $components = [
                     [
                         'type' => 'header',
@@ -148,7 +148,7 @@ class CloudApiImplementers extends BaseService
                             [
                                 'type' => 'image',
                                 'image' => [
-                                    'link' => 'https://www.whatsqunfa.com/pros/static/medias/images/photo4.png'
+                                    'link' => 'https://www.whatsqunfa.com/pros/static/medias/images/photo4.jpg'
                                 ],
                             ]
                         ]
